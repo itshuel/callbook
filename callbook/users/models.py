ab0dd0d1de9857e5436e948f644a42ad2ba2c920
huel <vldesign.des@gmail.com>
2023-02-02 08:48:43 -0500

    start add db

--- callbook/users/models.py
+++ callbook/users/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 
 class Person(models.Model):
-    name = models.CharField(max_length=30, blank=False, verbose_name='Имя пользователя')
-    surname = models.CharField(max_length=30, blank=False, verbose_name='Фамилия пользователя')
-    date = models.DateField(blank=False, verbose_name='Дата рождения пользователя')
-    status = models.CharField(max_length=15, blank=False, verbose_name='Статус пользователя')
-    phonenumber = models.CharField(max_length=11, unique=True, null=False, blank=False, verbose_name='Номер телефона пользователя')
-    info = models.TextField(max_length=100, blank=False, verbose_name='Информация о сотруднике')
+    name = models.CharField(max_length=30, verbose_name='Имя пользователя')
+    surname = models.CharField(max_length=30, verbose_name='Фамилия пользователя')
+    date = models.DateField(verbose_name='Дата рождения пользователя')
+    status = models.CharField(max_length=15, verbose_name='Статус пользователя')
+    phonenumber = models.CharField(max_length=11, verbose_name='Номер телефона пользователя')
+    info = models.TextField(max_length=100, verbose_name='Информация о сотруднике')
 
     def __str__(self):
         return '{}: {} {}, {}, {}, {}'.format(self.status, self.surname, self.name, self.date, self.phonenumber, self.info)
